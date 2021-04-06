e3397ca67dfff2c9caa204987e689d2589eeff62
youhua <106305013@nccu.edu.tw>
2021-04-06 17:45:42 +0800

    feat: add 'edit' function

--- app.js
+++ app.js
@@ -88,6 +88,42 @@ app.post('/create', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/edit/:id', (req, res) => {
+  Restaurant.findOne({ id: req.params.id })
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+})
+
+app.post('/edit/:id', (req, res) => {
+  const id = req.body.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.findOne({ id: req.params.id })
+    .then(restaurant => {
+      restaurant.id = id
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
